9f6bbab0364e7ac41355af49acf5be605ad607c7
IBruno-Castro <brunocastroficial@gmail.com>
2020-07-27 19:45:35 -0300

    Added Logo image and new video button

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import nerdflix from '../../assets/nerdflix.png'
+import './Menu.css'
+
 
 function Menu() {
     return (
-        <header>
-            NerdFlix
-        </header>
-    );
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={nerdflix} alt="Logo da NerdFlix" />
+            </a>
+            <a className="ButtonLink" href="/">
+                Novo vídeo
+            </a>
+        </nav>
+    )
 }
 
 export default Menu;
